be337e99c493c4a935f389099e659176298571ee
dbach787 <dbachmeier@gmail.com>
2020-07-16 09:37:18 -0700

    a

--- battery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-batLevel=`acpi -b | grep -P -o '[0-9]+(?=%)' | head -n 1`
-
-if [ $batLevel -le 20 ]
-    then
-        DISPLAY=:0.0 /usr/bin/notify-send "Low battery" "Plug it in!!!!"
-fi
